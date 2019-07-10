b42e5e8fefa5f143a129e87a5c7283ef2a66bea7
Mohammed Osama Mohamed Sayed Ahmed <mohamedosama914@users.noreply.github.com>
2019-07-10 17:30:44 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<frameset rows="*" cols="25%">
+  <frame frameborder=0 src="https://previewofflineinstaller.github.io/Download.New.Google.Chrome.Offline.Installer/HomePages/index.html" name="dot_tk_frame_content" scrolling="auto" noresize="100">
+  
+</frameset>
+
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+?>
+
+</html>
